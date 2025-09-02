9dd05b9ad4336b6ad2362be2f063839c5bae8e6a
Neil Jhon Dairo <njdairo13539@liceo.edu.ph>
2025-09-02 10:40:56 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
         String letterGrade = calculator.getLetterGrade(average);
         
         calculator.displayResult(studentName, average);
-        System.out.println();
         calculator.displayResult(studentName, average, letterGrade);
     }
 }
