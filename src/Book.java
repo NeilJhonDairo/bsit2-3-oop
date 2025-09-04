7f87a80678de41f7162f2375121eea3710e20ecd
Neil Jhon Dairo <njdairo13539@liceo.edu.ph>
2025-09-04 15:51:27 +0800

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,1 +1,46 @@
+import java.util.ArrayList;
 
+public class Book {
+    private String title;
+    private String author;
+    private ArrayList<Integer> ratings;
+    private int  totalBooks = 0;
+    private double averageRating;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.ratings = new ArrayList<>();
+        totalBooks++;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void displayInfo() {
+        String ratingLevel = getRatingLevel();
+        System.out.println("Book: " + title + " by " + author + ", Average Rating: " + averageRating + ", Level: " + ratingLevel);
+    }
+
+    public void setRating(double rating) {
+        if (rating >= 1 && rating <= 5) {
+            averageRating = rating;
+            System.out.println("Rating " + rating + " added successfully");
+        } else {
+            System.out.println("Error: Invalid rating: must be 1-5 stars");
+        }
+
+    }
+
+    private String getRatingLevel() {
+        if (averageRating >= 4.0) {
+            return "Good";
+        } else if (averageRating >= 3.0) {
+            return "Average";
+        } else {
+            return "Poor";
+        }
+    }
+}
