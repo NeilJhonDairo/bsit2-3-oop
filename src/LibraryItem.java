b3f0773782e2c9ef313a2b5ed466c29526e91173
Neil Jhon Dairo <njdairo13539@liceo.edu.ph>
2025-10-02 15:54:42 +0800

    Update LibraryItem.java

--- src/LibraryItem.java
+++ src/LibraryItem.java
@@ -1,1 +1,33 @@
+public abstract class LibraryItem {
+    protected String itemId;
+    protected String title;
+    protected String author;
+    protected boolean isCheckedOut;
+    protected String borrowerName;
+
+    public LibraryItem(String itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+        this.isCheckedOut = false;
+        this.borrowerName = "";
+    }
+
+    public String getItemInfo() {
+        return getItemType() + ": " + title + " by " + author;
+    }
+
+    public void checkOut(String borrowerName) {
+        this.isCheckedOut = true;
+        this.borrowerName = borrowerName;
+    }
+
+    public void checkIn() {
+        this.isCheckedOut = false;
+        this.borrowerName = "";
+    }
+
+    public abstract String getItemType();
+    public abstract double calculateLateFee(int daysLate);
+}
 
